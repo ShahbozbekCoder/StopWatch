25f45fb0877ecc3901993f192d34c12d0e22d0c6
Shahbozbek <shahbozbey99@gmail.com>
2025-03-24 17:43:19 +0500

    added NewsScreen

--- app/src/main/java/com/shahbozbek/stopwatch/navigation/AppNavigation.kt
+++ app/src/main/java/com/shahbozbek/stopwatch/navigation/AppNavigation.kt
@@ -1,6 +1,5 @@
 package com.shahbozbek.stopwatch.navigation
 
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
@@ -8,7 +7,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.shahbozbek.stopwatch.ui.stopwatch.StopWatchScreen
 import com.shahbozbek.stopwatch.ui.weather.WeatherScreen
-import kotlin.system.exitProcess
 
 @Composable
 fun AppNavigation(navHostController: NavHostController, paddingValues: PaddingValues) {
